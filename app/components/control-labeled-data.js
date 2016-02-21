2a0f8d4e44631b52814a18aab417136708fa4be9
superlou <lousimons@gmail.com>
2016-02-20 19:21:49 -0500

    Use bindValue instead of just bind to permit multiple bindings per control

--- app/components/control-labeled-data.js
+++ app/components/control-labeled-data.js
@@ -4,8 +4,8 @@ export default Ember.Component.extend({
   classNames: ['control-labeled-data'],
 
   value: Ember.computed('config', 'data', function() {
-    if (this.get('config.bind')) {
-      var value = this.get('data.' + this.get('config.bind'));
+    if (this.get('config.bindValue')) {
+      var value = this.get('data.' + this.get('config.bindValue'));
       if (value !== undefined) {
         return value;
       }
