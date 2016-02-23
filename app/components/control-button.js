b98ac80e80f9c4fbf5e62b0ad0d62184e7f52cf5
superlou <lousimons@gmail.com>
2016-02-22 21:22:31 -0500

    Cleaning up component

--- app/components/control-button.js
+++ app/components/control-button.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['control-button'],
+  tagName: 'button',
+
+  click: function() {
+    this.send('click');
+  },
 
   actions: {
     click: function() {
