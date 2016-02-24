2073d9075ce7a6ce2ab14ea089bd9f603f2a18fb
superlou <lousimons@gmail.com>
2016-02-23 21:18:17 -0500

    Allow activating buttons

--- app/components/control-button.js
+++ app/components/control-button.js
@@ -2,7 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['control-button'],
+  classNameBindings: ['isActive'],
   tagName: 'button',
+
+  isActive: Ember.computed('config', 'data', function() {
+      if (this.get('data.' + this.get('config.bindActive'))) {
+        return true;
+      } else {
+        return false;
+      }
+  }),
 
   click: function() {
     this.send('click');
